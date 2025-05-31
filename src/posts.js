9ca3d9af66938db9a348c3d9674ab4e8a310141b
Shiv Mehra <mehrashiv@yahoo.com>
2025-05-31 08:38:35 -0700

    more stuff

--- src/posts.js
+++ src/posts.js
@@ -1,19 +1,35 @@
 // posts.js - Blog posts data storage
 export const blogPosts = [
   {
-    id: 2,
+    id: 4,
+    title: "My Eel drawing",
+    date: "2025-05-30",
+    content: "My drawing challenge for the week - Eel",
+    image: "https://minio-api.rayanmehra.com/public/rayan/IMG_8525.JPEG",
+    tags: ["School Stuff"]
+  },
+  {
+    id: 3,
     title: "LEGO Robot",
     date: "2025-05-28",
-    content: "My and dad I a built a LEGO robot...",
+    content: "My dad and I spent a whole weekend on this robot ",
     image: "https://minio-api.rayanmehra.com/public/rayan/legorobot.JPEG",
     tags: ["LEGO"]
   },
   {
-    id: 1,
+    id: 2,
     title: "I am a super reader",
     date: "2025-05-22",
-    content: "I read of bunch a books and then some more...",
+    content: "I finished all the picture books in my book reading challenge",
     image: "https://minio-api.rayanmehra.com/public/rayan/superreader.JPEG",
+    tags: ["School Stuff"]
+  },
+  {
+    id: 1,
+    title: "Read all my picture books",
+    date: "2025-05-02",
+    content: "For every book I read, I got a magnet. Check out all the books I read at school",
+    image: "https://minio-api.rayanmehra.com/public/rayan/IMG_8526.JPEG",
     tags: ["School Stuff"]
   },
   
